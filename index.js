c7fdfb77add83e9c5885a341788cbaeab96b3606
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-12 22:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
-
-function holidayCountdown(number, holiday) {
-  return (`It's ${number} days until ${holiday}!`)
-}
